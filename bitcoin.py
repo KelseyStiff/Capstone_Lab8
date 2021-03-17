bee27cdd11945439023333a3ee2832c4504eed2c
kelseystiff <kelsey>
2021-03-17 13:47:54 -0500

    fixed test issue

--- bitcoin.py
+++ bitcoin.py
@@ -10,21 +10,21 @@ def get_bitcoin_amount():
     return float(input('Enter the number of bitcoin: '))
 
 def convert_amount_to_bitcoin(amount):
-    exchange_rate = get_exchange_rate()
-    converted = convert(amount, exchange_rate)
-    return converted
+    exchange_rate = get_exchange_rate(amount)
+    # converted = convert(amount, exchange_rate)
+    return exchange_rate
 
-def get_exchange_rate():
+def get_exchange_rate(bitcoin):
+    data = retrieve_data()
+    dollars_exchange_rate = data['bpi']['USD']['rate_float']
+    bitcoin_value_in_dollars = bitcoin * dollars_exchange_rate
+    return bitcoin_value_in_dollars
+
+def retrieve_data():
     coindesk_url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     response = requests.get(coindesk_url)
     data = response.json()
-    print(data)
-    dollars_exchange_rate = data['bpi']['USD']['rate_float']
-    return dollars_exchange_rate
-
-def convert(bitcoin, exchange_rate):
-    bitcoin_value_in_dollars = bitcoin * exchange_rate
-    return bitcoin_value_in_dollars
+    return data
 
 def display_results(bitcoin, bitcoin_value_in_dollars):
     print(f'{bitcoin} is equal to {bitcoin_value_in_dollars}')
--- test_bitcoin.py
+++ test_bitcoin.py
@@ -4,15 +4,46 @@ from unittest.mock import patch
 
 import bitcoin
 
-
 class TestBitcoin(TestCase):
-    @patch('bitcoin.get_exchange_rate')
+    @patch('bitcoin.retrieve_data')
     def test_(self, mock_rates):
-        mock_rate = 123.4567
-        example_api_response = {'time': {'updated': 'Mar 17, 2021 16:59:00 UTC', 'updatedISO': '2021-03-17T16:59:00+00:00', 'updateduk': 'Mar 17, 2021 at 16:59 GMT'}, 'disclaimer': 'This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org', 'chartName': 'Bitcoin', 'bpi': {'USD': {'code': 'USD', 'symbol': '&#36;', 'rate': '55,341.0833', 'description': 'United States Dollar', 'rate_float': mock_rate}, 'GBP': {'code': 'GBP', 'symbol': '&pound;', 'rate': '39,851.1694', 'description': 'British Pound Sterling', 'rate_float': 39851.1694}, 'EUR': {'code': 'EUR', 'symbol': '&euro;', 'rate': '46,489.2217', 'description': 'Euro', 'rate_float': 46489.2217}}}
+        mock_rate = 10
+        example_api_response = {
+                                "time": {
+                                "updated": "Mar 17, 2021 18:26:00 UTC",
+                                "updatedISO": "2021-03-17T18:26:00+00:00",
+                                "updateduk": "Mar 17, 2021 at 18:26 GMT"
+                                },
+                                "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
+                                "chartName": "Bitcoin",
+                                "bpi": {
+                                "USD": {
+                                "code": "USD",
+                                "symbol": "&#36;",
+                                "rate": "56,635.0074",
+                                "description": "United States Dollar",
+                                "rate_float": mock_rate
+                                },
+                                "GBP": {
+                                "code": "GBP",
+                                "symbol": "&pound;",
+                                "rate": "40,868.1045",
+                                "description": "British Pound Sterling",
+                                "rate_float": 40868.1045
+                                },
+                                "EUR": {
+                                "code": "EUR",
+                                "symbol": "&euro;",
+                                "rate": "47,557.1520",
+                                "description": "Euro",
+                                "rate_float": 47557.152
+                                }
+                                }
+                                }        
         mock_rates.side_effect = [example_api_response]
-        converted = bitcoin.convert(100, mock_rate)
-        expected = 12345.67
+        
+        converted = bitcoin.get_exchange_rate(100)
+        expected = 1000
         self.assertEqual(expected, converted)
 
 
